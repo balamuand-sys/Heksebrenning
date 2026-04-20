6bc898940079836aca5b808cb621ca37ea648150
balamuand-sys <balamuand@gmail.com>
2026-04-20 10:47:30 +0200

    Update agenda.js

--- src/data/agenda.js
+++ src/data/agenda.js
@@ -3,7 +3,7 @@ export const agendaData = [
     date: "Torsdag 30.04.26",
     items: [
       { id: 1, time: "13:30", title: "Matbit", location: "Hamburg Hbf", budget: null, icon: "Beer", details: "Vi reiser fra flughafen med S-bahn til Hamburg haubtbahnhof. Vi får i oss en matbit på togstasjonen i Hamburg før videre reise. Evt. Innkjøp av snacks til turen." },
-      { id: 2, time: "14:23", title: "Avreise til Goslar", location: "Hamburg Hbf", budget: null, icon: "Train", details: "Vi reiser med ICE 1187 kl. 14:23 fra spor 12C-F. Toget går i retning Frankfurt. Vi hopper av i Hannover 15:38 og bytter til regiontog RE10 som skal til Bad Harzburg (spor 7). Seks stopp senere ankommer vi Goslar. Reisekomiteen gjør oppmerksom på at wegbier er helt greit å ha med om bord ICE-tog, men ikke regiontogene." },
+      { id: 2, time: "14:23", title: "Avreise til Goslar", location: "Hamburg Hbf", budget: null, icon: "Train", details: "Vi reiser med ICE 1187 kl. 14:23 fra spor 12C-F. Toget går i retning Frankfurt. Vi hopper av i Hannover 15:38 og bytter til S4 som skal til Hildesheim kl. 16:19. Siste etappe mot Goslar går fra Hildesheim kl. 17:14. Reisekomiteen gjør oppmerksom på at wegbier er helt greit å ha med om bord ICE-tog, men ikke regiontogene." },
       { id: 3, time: "~17:00", title: "Innsjekking", location: "Hotel der Achtermann", budget: null, icon: "MapPin", details: "Innsjekk og omstilling på hotellet før kveldens festligheter. Hotellet ligger sentralt i den UNESCO-vernede byen Goslar." },
       { id: 4, time: "~18:00", title: "Heksebrenning", location: "Goslar", budget: "€ 35 (bargeld)", icon: "Flame", details: "Vi tar del i kveldens festligheter. Det ryktes om matboder, live-musikk, store bål og god stemning. Hver deltaker får et budsjett på € 35 utdelt i kontanter til eget forbruk av mat og drikke." }
     ]
@@ -11,7 +11,7 @@ export const agendaData = [
   {
     date: "Fredag 01.05.26",
     items: [
-      { id: 5, time: "10:30", title: "Frühschoppen", location: "Cafe am Markt, Goslar", budget: "€ 30", icon: "Beer", details: "...tyskerne har selvsagt både et ord, og kultur for, å drikke til frokost. Selv om fokuset er på drikke, serveres ofte en andre frokost, gjerne med Weißwurst, søt sennep og pretzel. Tog til Hamburg går 13:03." },
+      { id: 5, time: "10:30", title: "Frühschoppen", location: "Cafe am Markt, Goslar", budget: "€ 30", icon: "Beer", details: "...tyskerne har selvsagt både et ord, og kultur for, å drikke til frokost. Selv om fokuset er på drikke, serveres ofte en andre frokost, gjerne med Weißwurst, søt sennep og pretzel. Tog mot Hamburg går 12:36, med bytte i Kreiensen 13:11-13:31 og Hannover 14:23-14:36." },
       { id: 6, time: "13:03", title: "Avreise til Hamburg", location: "Goslar Stasjon", budget: null, icon: "Train", details: "Tog til Hamburg med bytte i Hannover 14:10-14:36. Tilbake til sivilisasjonen i Tysklands nest største by." },
       { id: 7, time: "~17:15", title: "Innsjekking", location: "Scandic Hamburg Emporio", budget: null, icon: "MapPin", details: "Innsjekking på hotellet vårt i Hamburg, beliggende ved Gänsemarkt." },
       { id: 8, time: "18:00", title: "Middag", location: "Oberhafen Kantine, Hamburg", budget: "€ 60", icon: "Utensils", details: "Meget tradisjonsrik restaurant hvor arbeiderne i havnen gjerne spiste. Bygget er skjevt og det serveres Labskaus." },
